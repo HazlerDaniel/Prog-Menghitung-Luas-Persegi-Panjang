3817d8730bb3cba1a83bb5ae527f443a9794851e
Daniel Xiha <40915588+HazlerDaniel@users.noreply.github.com>
2018-12-30 18:22:28 +0700

    Add files via upload

---
+++ menghitung luas persegi panjang.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	int p,l,ls=0;
+	clrscr();
+	printf("menghitung luas persegi panjang\n------------------");
+	printf("\n masukan panjang:");
+	scanf("%d",&p);
+	printf("\n masukan lebar:");
+	scanf("%d",&l);
+	luas=p*l;
+	printf("\n luas persegi panjang:%d",ls);
+}
